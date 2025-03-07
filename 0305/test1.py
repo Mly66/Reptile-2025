483ef68440f073ea0950a3abc018006287b6202c
Mly66 <3127328609@qq.com>
2025-03-07 09:16:32 +0800

    0305

--- 0305/test1.py
+++ 0305/test1.py
@@ -1,23 +1,15 @@
-print("Hello World")
+import requests
 
-print("Hello World")
-
-print("Hello World")
-
-print("Hello World")
-
-print("Hello World")
-
-print("Hello World")
-
-print("Hello World")
-
-print("Hello World")
-
-print("Hello World")
-
-print("Hello World")
-
-print("Hello World")
-
-print("Hello World")
+r = requests.get("https://www.baidu.com")
+print(r.status_code)
+try:
+    print(r.encoding)
+    print(r.apparent_encoding)
+    r.encoding = r.apparent_encoding
+    print(r.text)
+    print(r.headers)
+    print(type(r.headers))
+except:
+    print("请求失败")
+finally:
+    print("程序结束")
