3f814e1766b45ef818060cc9523d59ad683dc88f
Mly66 <3127328609@qq.com>
2025-04-09 08:32:14 +0800

    work 1

--- 0326/Test-bs4.py
+++ 0326/Test-bs4.py
@@ -17,19 +17,34 @@ def beautifulhtml(url):
         print(f"请求失败: {e}")
         return None
 
+def testpost():
+    url = 'http://httpbin.org/post'
+
+    data = {
+        'students': [
+            {'name': '张三', 'age': 18, 'grade': 'A', 'major': '计算机科学'},
+            {'name': '李四', 'age': 19, 'grade': 'B', 'major': '电子工程'},
+            {'name': '王五', 'age': 20, 'grade': 'A', 'major': '机械工程'},
+            {'name': '赵六', 'age': 18, 'grade': 'C', 'major': '数学'},
+            {'name': '钱七', 'age': 21, 'grade': 'B', 'major': '物理'}
+        ]
+    }
+
+    return requests.post(url, data=data)
 
 def main():
-    url = "https://python123.io/ws/demo.html"
-    title = beautifulhtml(url)
-    print(type(title))
-    match = re.search(r'[1-9]\d{5}', "BTI32746329874 TUS96743298756")
-    if match:
-        print(match.group(0))
-    pat = re.compile(r'[a-z]')
-    math = pat.search(title)
-    if math:
-        print(math.span())
+    # url = "https://python123.io/ws/demo.html"
+    # title = beautifulhtml(url)
+    # print(type(title))
+    # match = re.search(r'[1-9]\d{5}', "BTI32746329874 TUS96743298756")
+    # if match:
+    #     print(match.group(0))
+    # pat = re.compile(r'[a-z]')
+    # math = pat.search(title)
+    # if math:
+    #     print(math.span())
     # print(f"网页标题: {title}")
+    print(testpost().text)
 
 
 if __name__ == '__main__':
---
+++ 0409/work1.py
@@ -0,0 +1,79 @@
+﻿import os
+import requests
+
+def gethtml(url, key):
+    try:
+        r = requests.get(url, params=key)
+        print(r.status_code)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        return r.text
+    except:
+        print("error")
+        return None
+    finally:
+        print("success")
+
+def getTxt(txt, filename):
+    if os.path.exists(filename):
+        print(f"{filename} 已经存在，将会被覆盖")
+
+    folder = os.path.dirname(filename)
+    if folder and not os.path.exists(folder):
+        os.makedirs(folder)
+
+    with open(filename, 'w', encoding="utf-8") as f:
+        print(f"正在写入 {filename}")
+        f.write("获取网页内容如下\n")
+        f.write(txt)
+    print("success")
+    return filename
+
+
+def getpic(url, root):
+    path = os.path.join(root, url.split('/')[-1])
+
+    try:
+        if not os.path.exists(root):
+            os.mkdir(root)
+        if not os.path.exists(path):
+            r = requests.get(url)
+            with open(path, 'wb') as f:
+                f.write(r.content)
+            print("success")
+        else:
+            print("文件已存在")
+    except:
+        print("error")
+    finally:
+        print("nb")
+
+def getnames(url, key):
+    try:
+        r = requests.get(url, params=key)
+        print(r.status_code)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        return str(len(r.text))
+    except:
+        print("error")
+        return None
+    finally:
+        print("success")
+
+def main():
+    url = "http://www.baidu.com/s"
+    key = {"wd": "马璐瑶"}
+    html = gethtml(url, key)
+    getTxt(html, "./output/test.txt")
+
+    picurl = "https://wallpapercave.com/wp/mJRWj5T.jpg"
+    path = "./pics/"
+    getpic(picurl, path)
+
+    url = "http://www.so.com/s"
+    key = {"q": "马璐瑶"}
+    getnames(url, key)
+
+if __name__ == '__main__':
+    main()
