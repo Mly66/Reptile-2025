e3f225219c6a71ccd46e9a5029c466a66e46c32b
Mly66 <130681055+Mly66@users.noreply.github.com>
2025-05-14 01:39:23 +0000

    csv

--- 0514/info-test.py
+++ 0514/info-test.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import os
 
 url = "http://python123.io/ws/demo.html"
 r = requests.get(url)
@@ -14,3 +15,14 @@ with open("0514/0514.txt", "a") as file:
     for i in b.descendants:
         if i.name == "a" and 'href' in i.attrs:
             file.write(i.attrs['href'] + "\n") 
+
+if not os.path.exists("0514/nbmly"):
+    os.mkdir("0514/nbmly")
+# if not os.path.exists("0514/nbmly/test.txt"):
+#     os.mkdir("0514/nbmly/test.txt")
+with open("0514/nbmly/test.txt", "w") as file:
+    n = 1
+    for i in b.descendants:
+        if i.name == "a" and 'href' in i.attrs:
+            file.write(str(n) + i.attrs['href'] + "\n")
+            n = n + 1
