c79c08d0f47e33fa4c7f75b9000660233e4908f8
Mly66 <3127328609@qq.com>
2025-06-04 08:04:26 +0800

    0604

---
+++ 0604/re-test.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_html(url):
+    response = requests.get(url)
+    return response.text
+
+
+def get_soup(html):
+    return BeautifulSoup(html, 'html.parser')
+
+
+def main():
+    url = 'http://python123.io/ws/demo.html'
+    html = get_html(url)
+    soup = get_soup(html)
+    print(soup.prettify())
+    print(soup.title.string)
+
+
+if __name__ == '__main__':
+    main()
