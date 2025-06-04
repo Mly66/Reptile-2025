b8e1514e4c7ecc631086f0b049ae159f00d8612a
Mly66 <3127328609@qq.com>
2025-06-04 09:08:35 +0800

    Update re-test.py

--- 0604/re-test.py
+++ 0604/re-test.py
@@ -23,6 +23,48 @@ def save_text(path, filename, data):
             f.write(e.format(i, tag))
 
 
+def print_result(tags):
+    regex = 'h(e|ea)?d'
+    for tag in tags:
+        if re.search(regex, tag):
+            print(tag)
+    regex = r'^h'
+    for tag in tags:
+        if re.search(regex, tag):
+            print(tag)
+    regex = 'h..l'
+    for tag in tags:
+        if re.search(regex, tag):
+            print(tag)
+    regex = '[a-c]'
+    for tag in tags:
+        if re.search(regex, tag):
+            print(tag)
+    regex = '^[^abc]'
+    for tag in tags:
+        if re.search(regex, tag):
+            print(tag)
+    regex = '^[ti|he]..'
+    for tag in tags:
+        if re.search(regex, tag):
+            print(tag)
+    regex = '\w'
+    for tag in tags:
+        if re.search(regex, tag):
+            print(tag)
+    regex = 'a$'
+    for tag in tags:
+        if re.search(regex, tag):
+            print(tag)
+
+
+def check_ip(ip):
+    regex = '^(25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)){3}$'
+    if re.match(regex, ip):
+        return True
+    return False
+
+
 def main():
     url = 'http://python123.io/ws/demo.html'
     html = get_html(url)
@@ -35,8 +77,14 @@ def main():
     filename = 'data.txt'
 
     tags = [tag.name for tag in soup.find_all()]
-
+    print_result(tags)
     save_text(path, filename, tags)
+
+    ip = '192.168.127.552'
+    if check_ip(ip):
+        print("yes")
+    else:
+        print("no")
 
 
 if __name__ == '__main__':
